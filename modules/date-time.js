617c633417e8f2b15c6c3bcaa25c42ff05f24c41
AJ193 <119049613+AJ193@users.noreply.github.com>
2023-05-01 16:37:49 +0500

    fixing linter errors

--- modules/date-time.js
+++ modules/date-time.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 const ourLocalDate = luxon.DateTime.local().toLocaleString(luxon.DateTime.DATE_MED);
 const { hour } = luxon.DateTime.local().c;
 const minit = luxon.DateTime.local().c.minute;
